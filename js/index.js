886f19b47ddceb333b0fb260f1a9f96ec53f6c80
alanblee <alanbenlee12@gmail.com>
2020-03-10 13:14:51 -0700

    stopped page from refreshing and stopped propagation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+//prevent links from refreshing page
+
+document.querySelectorAll(".nav-link").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+  });
+});
+
+//stop propagation
+document.querySelector(".nested-p").addEventListener("click", e => {
+  console.log(e.target);
+});
+document.querySelector(".stop-prop2").addEventListener("click", e => {
+  console.log(e.target + " bubble");
+  e.stopPropagation();
+});
+document.querySelector(".stop-prop").addEventListener("click", e => {
+  console.log(e.target + " bubble2");
+});
